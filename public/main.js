15b99660bc762cb1e1141d674462bf943b64f469
Will <yh2689@nyu.edu>
2021-03-26 20:28:50 +0800

    front-end stuff

--- public/main.js
+++ public/main.js
@@ -1,7 +1,9 @@
+// Peer things //////////////////////////////////////////////////////////////////////
 var me = new Peer({
     host: '/',
     path: '/peerjs/broker',  // Yes, I am paying for the Twilio STUN servers...
-    config:{ 'iceServers': "SECRET :/",
+    // REMEMBER TO ERASE THIS BEFORE POSTING ON GITHUB
+    config:{ 'iceServers': [{'url': 'stun:global.stun.twilio.com:3478?transport=udp', 'urls': 'stun:global.stun.twilio.com:3478?transport=udp'}, {'url': 'turn:global.turn.twilio.com:3478?transport=udp', 'username': 'aead563a3f21314845e336c9a9301dd4576d48956c075cdf09f723679001e464', 'urls': 'turn:global.turn.twilio.com:3478?transport=udp', 'credential': 'Aif90NIKOSofnkKChbTGFDv5/o+CwoUsphz4y2+MHXw='}, {'url': 'turn:global.turn.twilio.com:3478?transport=tcp', 'username': 'aead563a3f21314845e336c9a9301dd4576d48956c075cdf09f723679001e464', 'urls': 'turn:global.turn.twilio.com:3478?transport=tcp', 'credential': 'Aif90NIKOSofnkKChbTGFDv5/o+CwoUsphz4y2+MHXw='}, {'url': 'turn:global.turn.twilio.com:443?transport=tcp', 'username': 'aead563a3f21314845e336c9a9301dd4576d48956c075cdf09f723679001e464', 'urls': 'turn:global.turn.twilio.com:443?transport=tcp', 'credential': 'Aif90NIKOSofnkKChbTGFDv5/o+CwoUsphz4y2+MHXw='}],
          'sdpSemantics': 'unified-plan' }
     });
 
@@ -34,3 +36,27 @@ me.on('connection', (conn) => {
     conn.on('data', (data) => {
         received.innerHTML += data+ "<br>";
     })})
+
+// Public Messaging /////////////////////////////////////////////////////////////////////
+let d = new Date
+var public_getMessage_lastDate = d.getTime()
+function public_getMessage(){
+    axios.get('/public_getPost')
+    .then(function (response) {
+      let new_messages = response.data
+      for (let i=0; i<new_messages.length; i++){
+          public_received.innerHTML += new_messages[i]+ "<br>";
+      }
+    })
+}
+
+function public_postMessage(){
+    axios.post('/public_post', {
+        'name': 'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFUCKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK',
+        'message': public_post.value
+      })
+    public_post.value = ""
+}
+
+public_receive_go.onclick = public_getMessage
+public_post_go.onclick = public_postMessage
--- server.js
+++ server.js
@@ -10,6 +10,18 @@ app.get("/", (request, response) => {
 
 // Handle requests (no editing outside this section)/////////////////////////////////////////////////////
 
+// Public Messaging 
+var public_posts = ["test 1","test 2"] //Temporary storage for public posts
+app.post("/public_post", (request, response) => {
+  console.log(request)
+  public_posts.push(request.body)
+  response.sendStatus(200);
+});
+
+app.get("/public_getPost", (request, response) => {
+  response.send(public_posts)
+});
+
 // Listen////////////////////////////////////////////////////////////////////////////////////////////////
 const listener = app.listen(process.env.PORT, () => {
   console.log("Server listening on port " + listener.address().port);
